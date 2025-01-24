6ab337093e191b7c8b0ac5dcd47f760c159b8d0c
jemalkopadze <kopadzejemo@gmail.com>
2025-01-24 17:38:01 +0400

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 import Pizza from "./Pizza";
 
 const App = () => {
-    return (
-        <div>
-            <h1>Padre Gino's - Order Now</h1>
-            <Pizza name= "Peperoni" description="pep. cheese, n stuff"/>
-            <Pizza name= "Peperoni" description="pep. cheese, n stuff"/>
-            <Pizza name= "Peperoni" description="pep. cheese, n stuff"/>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Padre Gino's - Order Now</h1>
+      <Pizza
+        name="Peperoni"
+        description="pep. cheese, n stuff"
+        image={"public/pizzas/pepperoni.webp"}
+      />
+      <Pizza
+        name="Peperoni"
+        description="pep. cheese, n stuff"
+        image={"public/pizzas/pepperoni.webp"}
+      />
+      <Pizza
+        name="Peperoni"
+        description="pep. cheese, n stuff"
+        image={"public/pizzas/pepperoni.webp"}
+      />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
